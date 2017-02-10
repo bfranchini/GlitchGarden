de53f79311b9e22dda0bb84975e9156964a8c839
Bruno <hondokitsune@gmail.com>
2017-02-09 22:04:46 -0500

    Added sad mosic on "You Lost" screen

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -4,14 +4,19 @@ using System.Collections;
 public class LoseCollider : MonoBehaviour {
 
 	private LevelManager levelManager;
+    private MusicManager musicManager;
 	
 	void Start()
 	{
-		levelManager = GameObject.FindObjectOfType<LevelManager>();
+		levelManager = FindObjectOfType<LevelManager>();
+        musicManager = FindObjectOfType<MusicManager>();
 	}
 	
 	void OnTriggerEnter2D()
-	{		
+	{	
+        //stop main music so we can play lose jingle
+        musicManager.StopMusic();	
+
 		if(levelManager)
 			levelManager.LoadLevel("03b lose");		
 			
